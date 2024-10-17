44f31ead12925eab7a9502be84469c7ba36705c2
NadaHamed <nadahamed723@gmail.com>
2024-10-17 05:14:35 +0300

    Initial Commit

---
+++ ValifyFrameWork/ValifyFrameWork.h
@@ -0,0 +1,18 @@
+//
+//  ValifyFrameWork.h
+//  ValifyFrameWork
+//
+//  Created by 2B on 17/10/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ValifyFrameWork.
+FOUNDATION_EXPORT double ValifyFrameWorkVersionNumber;
+
+//! Project version string for ValifyFrameWork.
+FOUNDATION_EXPORT const unsigned char ValifyFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ValifyFrameWork/PublicHeader.h>
+
+
